604ab43c019b3423dfff57232f9be9fe1e474a16
FormularSumo <formularsumo@outlook.com>
2020-09-13 13:02:21 +0100

    Started program
    Add class.lua, push.lua and StateMachine.lua
    
    main.lua now loads these files and opens a black window which can toggle fullscreen.

--- main.lua
+++ main.lua
@@ -1,1 +1,79 @@
+push = require 'push'
 
+Class = require 'class'
+
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/GameState'
+require 'states/HomeState'
+
+VIRTUAL_WIDTH = 1080
+VIRTUAL_HEIGHT = 1920
+
+function love.load()
+    -- app window title
+    love.window.setTitle('Star Wars Force Collection Remake')
+
+    -- load retro fonts
+    font50 = love.graphics.newFont(50)
+    font80 = love.graphics.newFont(80)
+    love.graphics.setFont(font80)
+    
+    -- initialize state machine with all state-returning functions
+    gStateMachine = StateMachine {
+        ['home'] = function() return HomeState() end,
+        ['game'] = function() return GameState() end,
+    }
+    gStateMachine:change('home')
+
+    math.randomseed(os.time()) --Randomises randomiser each time program is run. 
+
+    -- initialize our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, 0, 0, {
+        vsync = true,
+        fullscreen = true,
+        resizable = true
+    })
+
+    love.keyboard.keysPressed = {}
+end
+
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+
+    --Escape exits fullscreen
+    if key == 'escape' then
+        love.window.setFullscreen(false)
+        love.window.maximize()
+    end
+    --F11 toggles between fullscreen and maximised
+    if key == 'f11' then
+        if love.window.getFullscreen() == false then
+            love.window.setFullscreen(true)
+        else
+            love.window.setFullscreen(false)
+            love.window.maximize()
+        end
+    end
+end
+
+
+function love.update(dt)
+    gStateMachine:update(dt)
+    love.keyboard.keysPressed = {}
+end
+
+
+function love.draw()
+    push:start()
+
+    gStateMachine:render()
+
+    push:finish()
+end
---
+++ states/HomeState.lua
@@ -0,0 +1,16 @@
+GameState = Class{__includes = BaseState}
+
+PIPE_SPEED = 60
+PIPE_WIDTH = 70
+PIPE_HEIGHT = 288
+
+BIRD_WIDTH = 38
+BIRD_HEIGHT = 24
+
+function GameState:init()
+
+end
+
+function GameState:update(dt)
+    
+end
