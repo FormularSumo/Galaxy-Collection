11c80c855638ee1da34a9c336f0a2b1da7bdcca8
FormularSumo <formularsumo@outlook.com>
2020-09-20 13:59:51 +0100

    Create Character stats.lua

---
+++ Characters/Character stats.lua
@@ -0,0 +1,11 @@
+AhsokaS7 = {
+    ['attack'] = 19254,
+    ['defense'] = 16320,
+    ['evade'] = 0.1,
+}
+
+AnakinF3 = {
+    ['attack'] = 18524,
+    ['defense'] = 17354,
+    ['evade'] = 0.05,
+}
