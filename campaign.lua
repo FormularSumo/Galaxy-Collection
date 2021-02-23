f42d4e54192da088de66e4a168a0bbd518268ba7
FormularSumo <chewy.h@live.co.uk>
2021-02-23 15:14:53 +0000

    Add Endor background and improve video player
    https://pericror.com/zoom-background/star-wars-zoom-background/
    Endor background isn't used yet but works fine if I manually set it. Looks good, except button text will need to be white.
    Videos have been moved to Backgrounds folder and videos folder deleted. All backgrounds are saved as the same name now when they're created, and with the help of a couple other variable in the same table (background type - video/photo and seek - for where to rewind back) a single background manager in main.lua manages all backgrounds. It pauses/plays videos on loose focus (if setting is on), loops when they reach end of video and draws them.

--- campaign.lua
+++ campaign.lua
@@ -17,6 +17,9 @@ function battle1() --The 1st level of the game campaign, will in future be all E
     P2_deck_cards[15] = 'BobaFett'
     P2_deck_cards[16] = 'Greedo'
     P2_deck_cards[17] = 'B2SuperBattleDroid'
+    background['Background'] = love.graphics.newVideo('Backgrounds/Sand Dunes.ogv')
+    background['Type'] = 'video'
+    background['Seek'] = 2
 
     gStateMachine:change('game')
 end
