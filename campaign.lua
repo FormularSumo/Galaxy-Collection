102d65786b874346eea10233cd9f33941e42e7ac
FormularSumo <chewy.h@live.co.uk>
2021-03-04 19:17:49 +0000

    Add Endor level with all Ewoks

--- campaign.lua
+++ campaign.lua
@@ -1,4 +1,4 @@
-function battle1() --The 1st level of the game campaign, will in future be all Ewoks I think, currently these characters to make an interesting fight vs pre-built deck
+function maxed_dark_side() --Example level to make an interesting fight vs pre-built deck
     P2_deck_cards[0] = 'Maul'
     P2_deck_cards[1] = 'CountDooku'
     P2_deck_cards[2] = 'DarthSidious'
@@ -21,11 +21,19 @@ function battle1() --The 1st level of the game campaign, will in future be all E
     gStateMachine:change('game',{'Sand Dunes', 'video', 2, 0, 0, 0})
 end
 
-function throne_room() --The 1st level of the game campaign, will in future be all Ewoks I think, currently these characters to make an interesting fight vs pre-built deck
+function throne_room()
     P2_deck_cards[1] = 'RoyalGuard'
     P2_deck_cards[2] = 'DarthSidious'
     P2_deck_cards[3] = 'DarthVader'
     P2_deck_cards[4] = 'RoyalGuard'
    
     gStateMachine:change('game',{'Death Star Control Room', 'photo', 0, 1, 1 ,1})
+end
+
+function endor()
+    for i = 0, 17 ,1 do
+        P2_deck_cards[i] = 'Ewok'
+    end
+   
+    gStateMachine:change('game',{'Endor', 'photo', 0, 1, 1 ,1})
 end
