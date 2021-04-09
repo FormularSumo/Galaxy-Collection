30d7e6d53a2f502d3c3104b20e087a05b14db2a4
FormularSumo <chewy.h@live.co.uk>
2021-04-09 23:25:01 +0100

    Cards only swap positions when mouse is released

--- Card editor.lua
+++ Card editor.lua
@@ -25,36 +25,38 @@ function Card_editor:update_position(number,column,row)
 end
 
 function Card_editor:update()
-    if mouseDown and mouseLastX > self.x and mouseLastX < self.x + self.width and mouseLastY > self.y and mouseLastY < self.y + self.height and mouseTrapped == false then
+    if mouseDown and mouseLastX > self.x and mouseLastX < self.x + self.width and mouseLastY > self.y and mouseLastY < self.y + self.height then
         self.clicked = true
-        mouseTrapped = self.number
-        self.clicked_positionX = mouseLastX - self.x
-        self.clicked_positionY = mouseLastY - self.y
-    elseif mouseDown and mouseLastX > self.x and mouseLastX < self.x + self.width and mouseLastY > self.y and mouseLastY < self.y + self.height and not mouseTrapped2 and mouseTrapped ~= self.number then
-        self.clicked = true
-        mouseTrapped2 = self.number
+        if mouseTrapped == false then
+            mouseTrapped = self.number
+            self.clicked_positionX = mouseLastX - self.x
+            self.clicked_positionY = mouseLastY - self.y
+        elseif mouseTrapped ~= self.number then
+            mouseTrapped2 = self.number
+        end
     end
-    if self.clicked == true and mouseDown and mouseTrapped == self.number then
-        self.x = mouseLastX - self.clicked_positionX
-        self.y = mouseLastY - self.clicked_positionY
-    elseif self.clicked == true and mouseDown and mouseTrapped2 == self.number then
-        temporary = Card_editor(self.name,self.row,self.column,self.number)
-        temporary2 = Card_editor(P1_deck[mouseTrapped].name,P1_deck[mouseTrapped].row,P1_deck[mouseTrapped].column,P1_deck[mouseTrapped].number)
+    if self.clicked == true then
+        if mouseDown then
+            if mouseTrapped == self.number then
+                self.x = mouseLastX - self.clicked_positionX
+                self.y = mouseLastY - self.clicked_positionY
+            end
+        else
+            if mouseTrapped2 == self.number then
+                temporary = Card_editor(self.name,self.row,self.column,self.number)
+                temporary2 = Card_editor(P1_deck[mouseTrapped].name,P1_deck[mouseTrapped].row,P1_deck[mouseTrapped].column,P1_deck[mouseTrapped].number)
 
-        P1_deck[self.number]:update_position(temporary2.number,temporary2.column,temporary2.row)
-        P1_deck_edit(temporary.number,temporary2.name)
+                P1_deck[self.number]:update_position(temporary2.number,temporary2.column,temporary2.row)
+                P1_deck_edit(temporary.number,temporary2.name)
 
-        P1_deck[mouseTrapped]:update_position(temporary.number,temporary.column,temporary.row)
-        P1_deck_edit(temporary2.number,temporary.name)
+                P1_deck[mouseTrapped]:update_position(temporary.number,temporary.column,temporary.row)
+                P1_deck_edit(temporary2.number,temporary.name)
 
-        temporary = nil
-        temporary2 = nil
-        mouseTrapped = 'none'
-        mouseTrapped2 = 'none'
-    elseif self.clicked == true then
-        self.clicked = false
-        self.x = ((VIRTUAL_WIDTH / 12) * self.column) + 22
-        self.y = ((VIRTUAL_HEIGHT / 6) * self.row + (self.height / 48))
+                temporary = nil
+                temporary2 = nil
+            end
+            self.clicked = false
+        end
     end
 end
 
