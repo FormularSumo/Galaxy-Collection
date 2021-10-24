24fa4400e779770453229e4a6b91daba7ac2e899
FormularSumo <chewy.h@live.co.uk>
2021-10-24 10:05:56 +0100

    Remove projectile delay code
    Could be added back in later but probably won't because looks bad

--- Projectile.lua
+++ Projectile.lua
@@ -21,21 +21,13 @@ function Projectile:init(x,y,finalx,finaly,projectile,team,xoffset,yoffset)
     self.y_distance = tonumber(self.finaly-self.y)
     self.angle = math.atan(self.y_distance/self.x_distance)
     if team == 2 then self.angle = self.angle + math.rad(180) end
-    -- self.delay = love.math.random(0,4.5) / 10 --Used for adding a random delay to to when projectiles are fired
-    -- self.timer = 0
 end
 
 function Projectile:update(dt)
-    -- if self.timer > self.delay then
-        self.x = self.x + (self.x_distance * dt) --/ (1-self.delay)
-        self.y = self.y + (self.y_distance * dt) --/ (1-self.delay)
-    -- else
-        -- self.timer = self.timer + dt
-    -- end
+    self.x = self.x + (self.x_distance * dt) --/ (1-self.delay)
+    self.y = self.y + (self.y_distance * dt) --/ (1-self.delay)
 end
 
 function Projectile:render()
-    -- if self.timer > self.delay then
-        love.graphics.draw(self.image,self.x,self.y,self.angle)
-    -- end
+    love.graphics.draw(self.image,self.x,self.y,self.angle)
 end
