1f3d6c04ddf360507f40fc697706e945a34ed383
FormularSumo <chewy.h@live.co.uk>
2024-08-23 01:23:59 +0100

    Remove old comment

--- ImageDecoderThread.lua
+++ ImageDecoderThread.lua
@@ -3,7 +3,7 @@ local running = true
 while running do
     local imagePath = love.thread.getChannel("imageDecoderQueue"):pop()
     if imagePath then --Check if there are any tasks left
-        love.thread.getChannel("imageDecoderOutput"):push({imagePath,love.image.newImageData(imagePath .. ".png")}) --Maybe change .png to be passed directly
+        love.thread.getChannel("imageDecoderOutput"):push({imagePath,love.image.newImageData(imagePath .. ".png")})
     else
         running = false
     end
