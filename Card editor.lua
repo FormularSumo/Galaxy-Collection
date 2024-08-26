FormularSumo/Galaxy-Collection

commit 1b638fd22df0c4c8d4fd52c306600295b3aef54b

Don't sort inventory if cards swapped in deck

Card editor.lua
@@ -55,7 +55,9 @@ function Card_editor:swap()
             P1_cards_edit(self.number,{self.name,self.level,self.evolution})
         end
 
-        sort_inventory()
+        if not (mouseTrapped.in_deck and self.in_deck) then
+            sort_inventory()
+        end
         return
     end
     self.clicked = false

commit c39017377cab4239fb204f8141d85507ef36ee46

Fix snapping between cards that have been swapped
Regression caused by https://github.com/FormularSumo/Star-Wars-Galaxy-Collection/commit/1b638fd22df0c4c8d4fd52c306600295b3aef54b and https://github.com/FormularSumo/Star-Wars-Galaxy-Collection/commit/9156293d05a4851534f1f81e2ca8b88a042433af so mostly applicable in 0.11

Card editor.lua
@@ -58,12 +58,13 @@ function CardEditor:swap()
                 self.name, self.level, self.evolution, self.imagePath, self.image = mouseTrapped.name, mouseTrapped.level, mouseTrapped.evolution, mouseTrapped.imagePath, mouseTrapped.image
                 P1strength = P1strength + characterStrength({self.name,self.level,self.evolution})
             end
+            gStateMachine.current:updateGui()
         else
             --Swap stats between the two card being swapped
             self.row, self.column, self.number, self.x, self.y, self.inDeck, mouseTrapped.row, mouseTrapped.column, mouseTrapped.number, mouseTrapped.x, mouseTrapped.y, mouseTrapped.inDeck = mouseTrapped.row, mouseTrapped.column, mouseTrapped.number, mouseTrapped.x, mouseTrapped.y, mouseTrapped.inDeck, self.row, self.column, self.number, self.x, self.y, self.inDeck
 
             if mouseTrapped.inDeck then
-                P1deck[mouseTrapped.number] = mouseTrapped --Update gui
+                P1deck[mouseTrapped.number] = mouseTrapped
                 P1deckEdit(mouseTrapped.number,{mouseTrapped.name,mouseTrapped.level,mouseTrapped.evolution},true)
             else --Only happens if not in sandbox
                 P1cardsEdit(mouseTrapped.number,{mouseTrapped.name,mouseTrapped.level,mouseTrapped.evolution},true)
@@ -85,6 +86,7 @@ function CardEditor:swap()
 
             if not (mouseTrapped.inDeck and self.inDeck) then
                 gStateMachine.current:loadCards()
+                gStateMachine.current:updateCardsOnDisplay()
                 if self.inDeck then
                     if self.level ~= nil then P1strength = P1strength + characterStrength({self.name,self.level,self.evolution}) end
                     if mouseTrapped.level ~= nil then P1strength = P1strength - characterStrength({mouseTrapped.name,mouseTrapped.level,mouseTrapped.evolution}) end
@@ -92,6 +94,8 @@ function CardEditor:swap()
                     if mouseTrapped.level ~= nil then P1strength = P1strength + characterStrength({mouseTrapped.name,mouseTrapped.level,mouseTrapped.evolution}) end
                     if self.level ~= nil then P1strength = P1strength - characterStrength({self.name,self.level,self.evolution}) end
                 end
+            else
+                gStateMachine.current:updateGui()
             end
         end
         mouseTrapped = false

main.lua
@@ -101,7 +101,7 @@ function love.load()
     keyHoldTimer = 0
     keyPressedTimer = love.timer.getTime()
     mouseLocked = false
-    sandbox = false
+    sandbox = true
     yscroll = 0
     rawyscroll = 0
 