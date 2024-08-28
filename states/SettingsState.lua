f1a90a21dc87678b60043ab0ef77a231f15a7b3f
FormularSumo <chewy.h@live.co.uk>
2024-08-28 23:55:12 +0100

    Fix crash Settings not using rgb table for sliders

--- states/SettingsState.lua
+++ states/SettingsState.lua
@@ -2,7 +2,7 @@ SettingsState = Class{__includes = BaseState}
 
 function SettingsState:init()
     gui[1] = Button(function() toggleSetting('FPS_counter') end,'FPS Counter',font80,nil,'centre',100)
-    gui[2] = Slider('centre',330,300,16,function(percentage) love.audio.setVolume(percentage) Settings['volume_level'] = percentage end,0.3,0.3,0.3,1,1,1,Settings['volume_level'],0.5,function() bitser.dumpLoveFile('Settings.txt',Settings) end)
+    gui[2] = Slider('centre',330,300,16,function(percentage) love.audio.setVolume(percentage) Settings['volume_level'] = percentage end,{0.3,0.3,0.3},{1,1,1},Settings['volume_level'],0.5,function() bitser.dumpLoveFile('Settings.txt',Settings) end)
     gui['VolumeLabel'] = Text('Volume',font80,'centre',360)
     gui[3] = Button(function() toggleSetting('videos') gui[3]:toggle() updateBackground() end,function() return 'Videos: ' .. tostring(Settings['videos']) end,font80,nil,'centre',574)
 
