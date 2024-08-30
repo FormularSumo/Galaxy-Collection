2461d4d29ab3ef6c3772a31e6d4c9312dbd1e2a7
FormularSumo <chewy.h@live.co.uk>
2024-08-31 00:12:54 +0100

    Fix positioning of Settings volume slider being 100 off

--- states/SettingsState.lua
+++ states/SettingsState.lua
@@ -3,7 +3,7 @@ SettingsState = Class{__includes = BaseState}
 function SettingsState:enter()
     gui[1] = Button(function() gStateMachine:change('HomeState',true,true) end,'Main Menu',font70,nil,'centre',50)
     gui[2] = Button(function() toggleSetting('FPS_counter') end,'FPS Counter',font80,nil,'centre',250)
-    gui[3] = Slider('centre',350,300,16,function(percentage) love.audio.setVolume(percentage) Settings['volume_level'] = percentage end,{0.3,0.3,0.3},{1,1,1},Settings['volume_level'],0.5,function() bitser.dumpLoveFile('Settings.txt',Settings) end)
+    gui[3] = Slider('centre',450,300,16,function(percentage) love.audio.setVolume(percentage) Settings['volume_level'] = percentage end,{0.3,0.3,0.3},{1,1,1},Settings['volume_level'],0.5,function() bitser.dumpLoveFile('Settings.txt',Settings) end)
     gui['VolumeLabel'] = Text('Volume',font80,'centre',480)
     gui[4] = Button(function() toggleSetting('videos') gui[4]:toggle() updateBackground() end,function() return 'Videos: ' .. tostring(Settings['videos']) end,font80,nil,'centre',664)
 
